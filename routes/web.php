323631c09092b5619f472a446fb7460bb4af55a5
aliihsan <aliihsansepar@gmail.com>
2021-08-07 19:32:30 +0300

    EF-9 | reviwer changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
