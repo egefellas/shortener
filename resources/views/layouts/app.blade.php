626ba7017ef8615c08f715426fb7eabfeaa004bf
aliihsan <aliihsansepar@gmail.com>
2021-08-07 19:28:44 +0300

    EF-9 | reviwer changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
     <style>
-        body{
+        body {
             margin:0;
             padding:0;
         }
