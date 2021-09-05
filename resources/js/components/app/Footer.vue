b78c468011b08adcba1b79b26ee69da856f87b7a
aliihsan separ <aliihsan.separ@mediaclick.com.tr>
2021-09-05 16:20:44 +0300

    EF-11 | reviwer request changes

--- resources/js/components/app/Footer.vue
+++ resources/js/components/app/Footer.vue
@@ -9,10 +9,12 @@
                 You can read more and make your cookie choices here.
                 By continuing to use this site you are giving us your consent to do this.
             </div>
-            <div class="py-3 px-6 inline-block"><a
-                :href="privacy_link"
-                class="underline text-white text-opacity-50">Learn
-                More</a></div>
+            <div class="py-3 px-6 inline-block">
+                <a
+                    :href="privacy_link"
+                    class="underline text-white text-opacity-50">Learn
+                    More</a>
+            </div>
             <button
                 class="w-24 h-10 border-2 rounded inline-block font-extrabold px-0 mt-3"
                 @click="addGotItCookie">GOT IT
